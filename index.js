9acece0a9aca34d4ccdbc6be1f82aeb97d668e68
Renzo Diaz <renzodiaz554@gmail.com>
2024-06-20 12:24:06 -0500

    feat: filling the overview template with cards

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 const http = require("http");
 const fs = require("fs");
 
+const replaceTemplate = (template, product) => {
+  let output = template
+    .replace(/{%ID%}/g, product.id)
+    .replace(/{%PRODUCT_NAME%}/g, product.productName)
+    .replace(/{%IMAGE%}/g, product.image)
+    .replace(/{%FROM%}/g, product.from)
+    .replace(/{%NUTRIENTS%}/g, product.nutrients)
+    .replace(/{%QUANTITY%}/g, product.quantity)
+    .replace(/{%PRICE%}/g, product.price)
+    .replace(/{%DESCRIPTION%}/g, product.description);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const productData = JSON.parse(data);
+const products = JSON.parse(data);
 
 console.log("\n========HTTP SERVER========");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  //Overview page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
-  } else if (pathName === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = products.map((product) => replaceTemplate(tempCard, product)).join("");
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+  }
+
+  //Product page
+  else if (pathName === "/product") {
     res.end("This is the PRODUCT");
-  } else if (pathName === "/api") {
+  }
+
+  //API
+  else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
-  } else {
+  }
+
+  //Not found
+  else {
     res.writeHead(404, { "Content-type": "text/html", "custom-header": "hello-world" });
     res.end("<h1> Page not found! </h1>");
   }
